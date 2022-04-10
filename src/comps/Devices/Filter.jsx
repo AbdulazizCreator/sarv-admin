0472f2a1af8d3e72127bc0c81dff634514327429
abdulazizcreator <abdulazizprogrammer@gmail.com>
2022-04-11 00:56:31 +0500

    some changes

--- src/comps/Devices/Filter.jsx
+++ src/comps/Devices/Filter.jsx
@@ -18,16 +18,33 @@ const Filter = (props) => {
     standard_size: "",
     region: "",
     another: "",
+    meter_status_filter: "",
+    valve_status_filter: "",
+    last_online: "",
   });
 
   function handleFilter(e) {
+    let res = { ...filter };
+    res[e.target.name] = e.target.value;
+    if (e.target.name === "another") {
+      if (e.target.value === "meter_status_filter") {
+        res[e.target.value] = 1;
+        res["valve_status_filter"] = "";
+      } else if (e.target.value === "valve_status_filter") {
+        res[e.target.value] = 1;
+        res["meter_status_filter"] = "";
+      } else {
+        res["valve_status_filter"] = "";
+        res["meter_status_filter"] = "";
+      }
+    }
     setFilter({ ...filter, [e.target.name]: e.target.value });
-    props.getFilter({ ...filter, [e.target.name]: e.target.value });
+    props.getFilter(res);
   }
 
   return (
     <Grid container spacing={2}>
-      <Grid item lg={3} sm={6} xs={12}>
+      <Grid item lg={2} sm={6} xs={12}>
         <TextField
           id="outlined-search"
           label="Поиск"
@@ -37,7 +54,7 @@ const Filter = (props) => {
           fullWidth
         />
       </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
+      <Grid item lg={2} sm={12} xs={12}>
         <FormControl fullWidth>
           <InputLabel id="standard_size">Типоразмер счетчика</InputLabel>
           <Select
@@ -57,7 +74,7 @@ const Filter = (props) => {
           </Select>
         </FormControl>
       </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
+      <Grid item lg={2} sm={6} xs={12}>
         <FormControl fullWidth>
           <InputLabel id="region">Фильтр по региону</InputLabel>
           <Select
@@ -72,7 +89,7 @@ const Filter = (props) => {
             {regions.length !== 0 &&
               regions.map((item) => (
                 <MenuItem key={item.id} value={item.id}>
-                  {item.region}
+                  {item.region_name}
                 </MenuItem>
               ))}
           </Select>
@@ -90,13 +107,24 @@ const Filter = (props) => {
             onChange={handleFilter}
           >
             <MenuItem value="">Все</MenuItem>
-            <MenuItem value={10}>Счетчики, которые имеется ошибки</MenuItem>
-            <MenuItem value={20}>
-              Счетчики, которые не выходили на связь более 3 дней
+            <MenuItem value="meter_status_filter">
+              Счетчики, которые имеется ошибки
             </MenuItem>
-            <MenuItem value={30}>Счетчики c закрытым клапаном</MenuItem>
+            <MenuItem value="valve_status_filter">
+              Счетчики c закрытым клапаном
+            </MenuItem>
           </Select>
         </FormControl>
+      </Grid>
+      <Grid item lg={3} sm={6} xs={12}>
+        <TextField
+          id="outlined-search"
+          label="Счетчики, которые не выходили на связь более 3 дней"
+          type="number"
+          name="last_online"
+          onChange={handleFilter}
+          fullWidth
+        />
       </Grid>
     </Grid>
   );
